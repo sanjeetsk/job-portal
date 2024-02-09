16fc4f8bdad5fd915b23e0520a7db79abcbc8402
sanjeetsk <ssanjeetkumar67@gmail.com>
2024-02-09 23:59:23 +0530

    update

--- src/controllers/jobs.controller.js
+++ src/controllers/jobs.controller.js
@@ -1,5 +1,22 @@
 import JobModel from "../models/job.model.js";
 
-export default class JobController{
-    
+const jobModel = new JobModel();
+export default class JobController {
+
+    static async getAllJobs(req, res) {
+        const allJobs = Job.getAllJobs();
+        res.render('jobs', { jobs: allJobs });
+    }
+
+    static async getJobById(req, res) {
+        const jobId = req.params.id;
+        const job = jobModel.getJobById(jobId);
+        if (job) {
+            res.render('jobDetails', { job });
+        } else {
+            res.redirect('/404');
+        }
+    }
+
+
 }
--- src/models/job.model.js
+++ src/models/job.model.js
@@ -1,8 +1,8 @@
 
 
-export default class JobModel{
+export default class JobModel {
     constructor(
-        _id, 
+        _id,
         jobcategory,
         jobdesignation,
         joblocation,
@@ -13,40 +13,58 @@ export default class JobModel{
         numberofopenings,
         jobposted,
         applicants
-        ){
-            this._id = _id
-            this.jobcategory= jobcategory
-            this.jobdesignation = jobdesignation
-            this.joblocation = joblocation
-            this.companyname = companyname
-            this.salary = salary
-            this.applyby = applyby
-            this.skillsrequired = skillsrequired
-            this.numberofopenings = numberofopenings
-            this.jobposted = jobposted
-            this.applicants = applicants
-        }
+    ) {
+        this._id = _id
+        this.jobcategory = jobcategory
+        this.jobdesignation = jobdesignation
+        this.joblocation = joblocation
+        this.companyname = companyname
+        this.salary = salary
+        this.applyby = applyby
+        this.skillsrequired = skillsrequired
+        this.numberofopenings = numberofopenings
+        this.jobposted = jobposted
+        this.applicants = applicants
+    }
 
-        static getAll(){
-            return jobs;
-        }
+    static getAll() {
+        return jobs;
+    }
 
-        static add(_id, jobcategory, jobdesignation, joblocation, companyname, salary, applyby, skillsrequired, numberofopenings, jobposted, applicants){
-            let newJob = new JobModel(
-                jobs.length+1,
-                jobcategory,
-                jobdesignation,
-                joblocation,
-                companyname,
-                salary,
-                applyby,
-                skillsrequired,
-                numberofopenings,
-                jobposted,
-                applicants
-            )
-            jobs.push(newJob)
+    static addJob(_id, jobcategory, jobdesignation, joblocation, companyname, salary, applyby, skillsrequired, numberofopenings, jobposted, applicants) {
+        let newJob = new JobModel(
+            jobs.length + 1,
+            jobcategory,
+            jobdesignation,
+            joblocation,
+            companyname,
+            salary,
+            applyby,
+            skillsrequired,
+            numberofopenings,
+            jobposted,
+            applicants
+        )
+        jobs.push(newJob)
+    }
+
+    static getJobById(id) {
+        return jobs.find(job => job.id === id);
+    }
+
+    static updateJob(id, updatedJob) {
+        const index = jobs.findIndex(job => job.id === id);
+        if (index !== -1) {
+            jobs[index] = { ...jobs[index], ...updatedJob };
+            return true;
         }
+        return false;
+    }
+
+    static deleteJob(id) {
+        jobs = jobs.filter(job => job.id !== id);
+    }
+
 }
 
 const jobs = [
@@ -58,14 +76,14 @@ const jobs = [
         "ABC Company",
         "20 LPA",
         "30th June 2024",
-        ["HTML","CSS","JavaScript","React","Node"],
+        ["HTML", "CSS", "JavaScript", "React", "Node"],
         5,
         "5th Feb 2024",
         0
     ),
     new JobModel(
         2,
-        non-tech,
+        non - tech,
         "Manager",
         "New York City",
         "XYZ Company",
